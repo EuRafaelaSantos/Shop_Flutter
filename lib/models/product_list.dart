57a7adcd189e9372afaf083b785612b124028089
Rafaela Santos <rafaelasantos.informatica@gmail.com>
2023-09-20 19:27:06 -0400

    fix: exibindo os produtos cadastrados no firebase

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:shop/data/dummy_data.dart';
+
 import 'package:shop/models/product.dart';
 import 'package:http/http.dart' as http;
 
 class ProductList with ChangeNotifier {
   final _url =
       'https://shop-flutter-4fe43-default-rtdb.firebaseio.com/products.json';
-  final List<Product> _items = dummyProducts;
+  final List<Product> _items = [];
 
   List<Product> get items => [..._items];
   List<Product> get favoriteItems =>
@@ -20,7 +20,21 @@ class ProductList with ChangeNotifier {
 
   Future<void> loadProducts() async {
     final response = await http.get(Uri.parse(_url));
-    print(jsonDecode(response.body));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((productId, productData) {
+      _items.add(
+        Product(
+          id: productId,
+          name: productData['name'],
+          description: productData['description'],
+          price: productData['price'],
+          imageUrl: productData['imageUrl'],
+          isFavorite: productData['isFavorite'],
+        ),
+      );
+    });
+    notifyListeners();
   }
 
   Future<void> saveProduct(Map<String, Object> data) {
