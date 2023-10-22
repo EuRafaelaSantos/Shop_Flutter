031242c0df9e50972a2acb37e7762beb1b003098
Rafaela Santos <rafaelasantos.informatica@gmail.com>
2023-10-22 16:36:31 -0400

    feat: Registrando Usuário

---
+++ lib/models/auth.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Auth with ChangeNotifier {
+  static const _url =
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCE5u2PrUBrWi_Mwt8U22-yrxRo0DLuWss';
+  Future<void> signup(String email, String password) async {
+    final response = await http.post(
+      Uri.parse(_url),
+      body: jsonEncode({
+        'email': email,
+        'password': password,
+        'returnSecureToken': true,
+      }),
+    );
+  }
+}
--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -4,24 +4,23 @@ import 'package:shop/components/auth_form.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(children: [
         Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             gradient: LinearGradient(
               colors: [
                 Color.fromRGBO(215, 117, 255, 0.5),
-                Color.fromRGBO(255, 188, 117, 0.9)
+                Color.fromRGBO(255, 188, 117, 0.9),
               ],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
           ),
         ),
-        Container(
+        SizedBox(
           width: double.infinity,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -38,22 +37,24 @@ class AuthPage extends StatelessWidget {
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20),
                   color: Colors.deepOrange.shade900,
-                  boxShadow: [
+                  boxShadow: const [
                     BoxShadow(
-                        blurRadius: 8,
-                        color: Colors.black26,
-                        offset: Offset(0, 2)),
+                      blurRadius: 8,
+                      color: Colors.black26,
+                      offset: Offset(0, 2),
+                    ),
                   ],
                 ),
-                child: Text(
+                child: const Text(
                   'Minha Loja',
                   style: TextStyle(
                     fontSize: 45,
                     fontFamily: 'Anton',
-                    color: Theme.of(context).accentTextTheme.headline6?.color,
+                    color: Colors.white,
                   ),
                 ),
-              )
+              ),
+              const AuthForm(),
             ],
           ),
         ),
