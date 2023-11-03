e80cee13ef766090062f2cb121600442059c4038
Rafaela Santos <rafaelasantos.informatica@gmail.com>
2023-11-03 10:52:15 -0400

    feat: associando Favoritos a Usuário

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
+  static const user_favorites_url =
+      'https://shop-flutter-4fe43-default-rtdb.firebaseio.com/userFavorites';
   static const productBaseUrl =
       'https://shop-flutter-4fe43-default-rtdb.firebaseio.com/products';
   static const orderBaseUrl =
